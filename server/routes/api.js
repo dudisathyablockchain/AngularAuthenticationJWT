4715c47cec645d0029f6c9d6f3032c398776f0a1
dudisathyablockchain <dudisathya4u@gmail.com>
2018-11-26 17:24:13 +0530

    Backend committed

---
+++ server/routes/api.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const db = "mongodb://userdudisathya4u:passworddudisathya4u@ds117334.mlab.com:17334/dudisathya4ueventsdb";
+
+mongoose.connect(db, err => {
+    if(err) {
+        console.error('Error!' + err);
+    } else {
+        console.log('Connected to mongodb');
+    }
+});
+
+router.get('/', (req,res) => {
+    res.send('From API route');
+});
+
+router.post('/register', (req,res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registeredUser) => {
+        if(error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    });
+});
+
+router.post('/login', (req, res) => {
+    let userData = req.body;
+
+    User.findOne({email: userData.email}, (error, user) => {
+        if(error) {
+            console.log(error);
+        } else {
+            if(!user) {
+                res.status(401).send('Invalid email!!!');
+            } else if (user.password !== userData.password) {
+                res.status(401).send('Invalid password!!!');
+            } else {
+                res.status(200).send(user);
+            }
+        }
+    });
+});
+
+module.exports = router;
